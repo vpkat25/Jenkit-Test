4b091d76873537b644f164e4be7d708f643dd205
Vivek <vivekspatil25@gmail.com>
2025-10-07 22:23:28 +0530

    Jenkin Testing 2

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("This is java program");
         System.out.println("Output from java program");
         System.out.println(new Date());
+        System.out.println("Jenkin Tested");
     }
 }
