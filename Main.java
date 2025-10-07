5c84d8909891ef3aee11940b06fbc96cb72aa965
Vivek <vivekspatil25@gmail.com>
2025-10-07 22:19:13 +0530

    Testing git

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println("This is java program");
+        System.out.println("Output from java program");
+        System.out.println(new Date());
+    }
+}
